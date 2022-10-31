4e1aa72c16ae919d2b8745a8a264ad0d42e9c982
thekatzdev <thekatzdev@gmail.com>
2022-11-01 00:33:22 +0100

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+
+
+#let's find some files
+
+
+files = []
+
+for file in os.listdir():
+    if file == "encrypt.py" or file == "decrypt.py" or file == "thekey.key":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+
+print(files)
+
+
+with open("thekey.key", "rb") as key:
+    secretkey = key.read()
+
+
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_decrypted = Fernet(secretkey).decrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_decrypted)
